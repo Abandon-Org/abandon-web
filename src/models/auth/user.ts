30a7c0fc8f389613d216ca3d7aa95cd04af97b48
yycxx <yyi99yy@163.com>
2024-01-11 00:14:41 +0800

    feat(feat): aaaaa

--- src/models/auth/user.ts
+++ src/models/auth/user.ts
@@ -1,74 +1,117 @@
 import { listUsers } from '@/services/user';
+import { Effect, Reducer } from 'umi';
 
 interface UserMap {
-    [key: string]: any;
+  [key: string]: any;
+}
+
+interface UserModelType {
+  namespace: string;
+  state: {
+    currentUser: any;
+    userList: any[];
+    currentUserList: any[];
+    userMap: UserMap;
+    userNameMap: UserMap;
+    activities: any[];
+    operationLog: any[];
+    project_count: number;
+    case_count: number;
+    user_rank: number;
+    total_user: number;
+    weekly_case: any[];
+    followPlan: any[];
+  };
+  effects: {
+    fetchUserActivities: Effect;
+    fetchUserRecord: Effect;
+    updateUser: Effect;
+    deleteUser: Effect;
+    fetchUserList: Effect;
+    getGithubToken: Effect;
+    avatar: Effect;
+    queryUserStatistics: Effect;
+    queryFollowTestPlanData: Effect;
+    fetchCurrent: Effect;
+  };
+  reducers: {
+    save: Reducer<any>;
+    saveCurrentUser: Reducer<any>;
+    changeNotifyCount: Reducer<any>;
+  };
 }
 
 const getUserMap = (data: any[]) => {
-    const temp: UserMap = {};
-    const userNameMap: { [key: string]: string } = {};
-    data.forEach(item => {
-        temp[item.id] = item;
-        userNameMap[item.id] = item.name;
-    });
-    return { userMap: temp, userNameMap };
-}
-
-interface UserModel {
-    namespace: string;
-    state: {
-        currentUser: any;
-        userList: any[];
-        currentUserList: any[];
-        userMap: UserMap;
-        userNameMap: { [key: string]: string };
-        activities: any[];
-        operationLog: any[];
-        project_count: number;
-        case_count: number;
-        user_rank: number;
-        total_user: number;
-        weekly_case: any[];
-        followPlan: any[];
-    };
-    effects: {
-        fetchUserList: (
-            _: any,
-            { call, put }: { call: any, put: any }
-        ) => void;
-    };
-}
-
-const UserModel: UserModel = {
-    namespace: 'user',
-    state: {
-        currentUser: {},
-        userList: [],
-        currentUserList: [],
-        userMap: {},
-        userNameMap: {},
-        activities: [],
-        operationLog: [],
-        project_count: 0,
-        case_count: 0,
-        user_rank: 0,
-        total_user: 0,
-        weekly_case: [],
-        followPlan: [],
-    },
-    effects: {
-        *fetchUserList(_, { call, put }) {
-            const response = yield call(listUsers);
-            yield put({
-                type: 'save',
-                payload: {
-                    userList: response,
-                    currentUserList: response,
-                },
-            });
-            console.log(response);
-        },
-    },
+  const temp: UserMap = {};
+  const userNameMap: UserMap = {};
+  data.forEach((item) => {
+    temp[item.id] = item;
+    userNameMap[item.id] = item.name;
+  });
+  return { userMap: temp, userNameMap };
 };
 
+const UserModel: UserModelType = {
+  namespace: 'user',
+  state: {
+    currentUser: {},
+    userList: [],
+    currentUserList: [],
+    userMap: {},
+    userNameMap: {},
+    // 用户活动轨迹数据
+    activities: [],
+    operationLog: [],
+    project_count: 0,
+    case_count: 0,
+    user_rank: 0,
+    total_user: 0,
+    weekly_case: [],
+    // 关注的测试计划数据
+    followPlan: [],
+  },
+  effects: {
+    *fetchUserList(_, { call, put }) {
+      const response = yield call(listUsers);
+      if (response && response.data && Array.isArray(response.data.items)) {
+        const { userMap, userNameMap } = getUserMap(response.data.items);
+        yield put({
+          type: 'save',
+          payload: {
+            userList: response,
+            currentUserList: response,
+            userMap,
+            userNameMap,
+          },
+        });
+      }
+    },
+  },
+  reducers: {
+    save(state, { payload }) {
+      return { ...state, ...payload };
+    },
+
+    saveCurrentUser(state, action) {
+      localStorage.setItem('pityUser', JSON.stringify(action.payload || {}));
+      return { ...state, currentUser: action.payload || {} };
+    },
+
+    changeNotifyCount(
+      state = {
+        currentUser: {},
+      },
+      action,
+    ) {
+      return {
+        ...state,
+        currentUser: {
+          ...state.currentUser,
+          notifyCount: action.payload.totalCount,
+          unreadCount: action.payload.unreadCount,
+        },
+      };
+    },
+  },
+};
 export default UserModel;
--- src/pages/Backstage/Usermanage/index.tsx
+++ src/pages/Backstage/Usermanage/index.tsx
@@ -1,143 +1,253 @@
+import UserLink from '@/components/Button/UserLink';
+import AbandonPopConfirm from '@/components/Confirm/AbandonPopConfirm';
+import CONFIG from '@/consts/config';
+import { ConnectState } from '@/models/connect';
+import { UserOutlined } from '@ant-design/icons';
+import { PageContainer } from '@ant-design/pro-components';
+import {
+  Card,
+  Col,
+  Divider,
+  Form,
+  Input,
+  Modal,
+  Row,
+  Select,
+  Switch,
+  Table,
+  Tag,
+} from 'antd';
 import React, { useEffect, useState } from 'react';
-import { PageContainer } from "@ant-design/pro-components";
-import { Card, Col, Divider, Form, Input, Modal, Row, Select, Switch, Table, Tag } from "antd";
-import UserLink from "@/components/Button/UserLink";
-import CONFIG from "@/consts/config";
-import { UserOutlined } from "@ant-design/icons";
-import { connect } from "dva";
-import AbandonPopConfirm from "@/components/Confirm/AbandonPopConfirm";
-import UserModel from "@/models/auth/user";
+import { Dispatch, connect } from 'umi';
 
 const { Option } = Select;
 
-const UserInfo: React.FC<UserModel> = ({user, dispatch }) => {
-    const [modal, setModal] = useState<boolean>(false);
-    const [record, setRecord] = useState<any>({});
-    const [form] = Form.useForm();
+interface UserProps {
+  dispatch: Dispatch;
+  user: ConnectState['user'];
+  loading: ConnectState['loading'];
+}
 
-    const onSwitch = (value: boolean, id: number) => {
-        dispatch({
-            type: 'user/updateUser',
-            payload: {
-                id,
-                is_valid: value,
-            },
-        });
-    };
+const UserInfo: React.FC<UserProps> = ({ user, dispatch, loading }) => {
+  // 用户表单窗口, 默认关闭
+  const [modal, setModal] = useState(false);
+  const [record, setRecord] = useState({});
+  const [form] = Form.useForm();
+  // 获取所有用户信息
+  const fetchUserInfo = () => {
+    dispatch({
+      type: 'user/fetchUserList',
+    });
+  };
+  const onSwitch = (value, id) => {
+    dispatch({
+      type: 'user/updateUser',
+      payload: {
+        id,
+        is_valid: value,
+      },
+    });
+  };
 
-    const columns = [
-        {
-            title: 'id',
-            dataIndex: 'id',
-            key: 'id',
-        },
-        {
-            title: <span><UserOutlined /> 姓名</span>,
-            dataIndex: 'name',
-            key: 'name',
-            render: (_, record) => <UserLink marginLeft={4} user={record} />,
-        },
-        {
-            title: '✉ 邮箱',
-            dataIndex: 'email',
-            key: 'email',
-        },
-        {
-            title: '🎨 角色',
-            dataIndex: 'role',
-            key: 'role',
-            render: (role: number) => <Tag color={CONFIG.USER_ROLE_TAG[role]}>{CONFIG.USER_ROLE[role]}</Tag>,
-        },
-        {
-            title: '上次登录',
-            dataIndex: 'last_login_at',
-            key: 'last_login_at',
-        },
-        {
-            title: '🚫 是否启用',
-            dataIndex: 'is_valid',
-            key: 'is_valid',
-            render: (is_valid: boolean, record: any) => <Switch defaultChecked={is_valid} onChange={(e) => {
-                onSwitch(e, record.id);
-            }} />,
-        },
-        {
-            title: '🧷 操作',
-            key: 'ops',
-            render: (_, record: any) => (
-                <>
-                    <a onClick={() => {
-                        setRecord(record);
-                        form.setFieldsValue(record);
-                        setModal(true);
-                    }}>编辑</a>
-                    <Divider type="vertical" />
-                    <AbandonPopConfirm onConfirm={async () => {
-                        // await onDeleteUser(record.id);
-                    }} text="删除" title="你确定要删除该用户吗?" />
-                </>
-            ),
-        },
-    ];
+  const onDeleteUser = async (id) => {
+    const res = await dispatch({
+      type: 'user/deleteUser',
+      payload: {
+        id,
+      },
+    });
+    if (res) {
+      // 删除成功后重新获取用户信息
+      fetchUserInfo();
+    }
+  };
 
+  const columns = [
+    {
+      title: 'id',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: (
+        <span>
+          <UserOutlined /> 姓名
+        </span>
+      ),
+      dataIndex: 'name',
+      key: 'name',
+      render: (_, record) => <UserLink marginLeft={4} user={record} />,
+    },
+    {
+      title: '✉ 邮箱',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: '🎨 角色',
+      dataIndex: 'role',
+      key: 'role',
+      render: (role) => (
+        <Tag color={CONFIG.USER_ROLE_TAG[role]}>{CONFIG.USER_ROLE[role]}</Tag>
+      ),
+    },
+    {
+      title: '上次登录',
+      dataIndex: 'last_login_at',
+      key: 'last_login_at',
+    },
+    {
+      title: '🚫 是否启用',
+      dataIndex: 'is_valid',
+      key: 'is_valid',
+      render: (is_valid, record) => (
+        <Switch
+          defaultChecked={is_valid}
+          onChange={(e) => {
+            onSwitch(e, record.id);
+          }}
+        />
+      ),
+    },
+    {
+      title: '🧷 操作',
+      key: 'ops',
+      render: (_, record) => (
+        <>
+          <a
+            onClick={() => {
+              setRecord(record);
+              form.setFieldsValue(record);
+              setModal(true);
+            }}
+          >
+            编辑
+          </a>
+          <Divider type="vertical" />
+          <AbandonPopConfirm
+            onConfirm={async () => {
+              await onDeleteUser(record.id);
+            }}
+            text="删除"
+            title="你确定要删除该用户吗?"
+          />
+        </>
+      ),
+    },
+  ];
 
+  const { userList, currentUserList } = user;
 
-    const fetchUserInfo = async () => {
-        try {
-            // 使用 await 等待异步操作完成
-            await dispatch({
-                type: 'user/fetchUserList',
-            });
-        } catch (error) {
-            // 处理错误
-            console.error(error);
-        }
-    };
+  useEffect(() => {
+    fetchUserInfo();
+  }, []);
 
-    useEffect(() => {
-        fetchUserInfo();
-    }, []);
-
-
-    const { currentUserList } = user; // 从 user 属性中获取 currentUserList
-
-    return (
-        <PageContainer breadcrumb={null} title="用户管理页面">
-            <Card>
-                <Modal title="编辑用户" width={500} visible={modal} onCancel={() => setModal(false)}>
-                    <Form form={form} initialValues={record} {...CONFIG.LAYOUT}>
-                        <Form.Item label="姓名" name="name">
-                            <Input placeholder="输入用户姓名" />
-                        </Form.Item>
-                        <Form.Item label="邮箱" name="email">
-                            <Input placeholder="输入用户邮箱" />
-                        </Form.Item>
-                        <Form.Item label="角色" name="role">
-                            <Select>
-                                <Option key={0} value={0}>普通成员</Option>
-                                <Option key={1} value={1}>组长</Option>
-                                <Option key={2} value={2}>超级管理员</Option>
-                            </Select>
-                        </Form.Item>
-                    </Form>
-                </Modal>
-                <Row style={{ marginBottom: 12 }}>
-                    <Col span={18} />
-                    <Col span={6}>
-                        <Input.Search placeholder="输入用户邮箱或姓名" onChange={(e) => {
-                            // onSearch(e.target.value);
-                        }} />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={24}>
-                        <Table columns={columns} dataSource={currentUserList}/>
-                    </Col>
-                </Row>
-            </Card>
-        </PageContainer>
+  const onSearch = (value) => {
+    if (value === '') {
+      fetchUserInfo();
+      return;
+    }
+    const temp = userList.filter(
+      (item) =>
+        item.name.toLowerCase().indexOf(value.toLowerCase()) > -1 ||
+        item.email.toLowerCase().indexOf(value.toLowerCase()) > -1,
     );
+    dispatch({
+      type: 'user/save',
+      payload: {
+        currentUserList: temp,
+      },
+    });
+  };
+
+  const onSubmit = async () => {
+    const values = await form.getFieldsValue();
+    // 提交表单
+    const result = await dispatch({
+      type: 'user/updateUser',
+      payload: {
+        ...values,
+        id: record.id,
+      },
+    });
+    if (result) {
+      // 修改table状态
+      const temp = [...currentUserList];
+      currentUserList.forEach((v, idx) => {
+        if (v.id === record.id) {
+          temp[idx] = {
+            ...record,
+            ...values,
+          };
+        }
+      });
+      dispatch({
+        type: 'user/save',
+        payload: { currentUserList: temp },
+      });
+      // 请求成功后关闭对话框
+      setModal(false);
+    }
+  };
+
+  return (
+    <PageContainer breadcrumb={null} title="用户管理页面">
+      <Card>
+        <Modal
+          title="编辑用户"
+          width={500}
+          open={modal}
+          onCancel={() => setModal(false)}
+          onOk={onSubmit}
+        >
+          <Form form={form} initialValues={record} {...CONFIG.LAYOUT}>
+            <Form.Item label="姓名" name="name">
+              <Input placeholder="输入用户姓名" />
+            </Form.Item>
+            <Form.Item label="邮箱" name="email">
+              <Input placeholder="输入用户邮箱" />
+            </Form.Item>
+            <Form.Item label="角色" name="role">
+              <Select>
+                <Option key={0} value={0}>
+                  普通成员
+                </Option>
+                <Option key={1} value={1}>
+                  组长
+                </Option>
+                <Option key={2} value={2}>
+                  超级管理员
+                </Option>
+              </Select>
+            </Form.Item>
+          </Form>
+        </Modal>
+        <Row style={{ marginBottom: 12 }}>
+          <Col span={18} />
+          <Col span={6}>
+            <Input.Search
+              placeholder="输入用户邮箱或姓名"
+              onChange={(e) => {
+                onSearch(e.target.value);
+              }}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <Table
+              columns={columns}
+              dataSource={currentUserList}
+              loading={loading.effects['user/fetchUserList']}
+            />
+          </Col>
+        </Row>
+      </Card>
+    </PageContainer>
+  );
 };
 
-
-export default connect(({ user }) => ({ user }))(UserInfo);
+export default connect(({ user, loading }: ConnectState) => ({
+  user,
+  loading,
+}))(UserInfo);
